accdc5824ef98d1c0af473fe4157ec3ecb7128f1
James <jgambardella90@gmail.com>
2017-09-19 12:41:59 -0700

    removed framework and Gymx

--- Framework/gymx-ext/shortcodes/parts/vc_init.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Map Shortcodes ----------------------------------------------------------------------->
-
-require_once( plugin_dir_path( __FILE__ ) . 'timetable_vc.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'class-grid_vc.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'trainer-grid_vc.php' );
